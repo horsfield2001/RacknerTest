d8db4499e08e4dad18d4944e9f83ba01d35bc0a0
horsfield2001 <35241284+horsfield2001@users.noreply.github.com>
2020-08-12 23:42:23 -0700

    load saved image

--- RacknerTest/UserImageViewController.swift
+++ RacknerTest/UserImageViewController.swift
@@ -17,24 +17,42 @@ class UserImageViewController: UIViewController , UIImagePickerControllerDelegat
     @IBAction func ShowImagePicker(_ sender: UIButton) {
         imagePicker.allowsEditing = false
         imagePicker.sourceType = .photoLibrary
-            
+        
         present(imagePicker, animated: true, completion: nil)
     }
-
-   override func viewDidLoad() {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
         imagePicker.delegate = self
+        
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let imagePath = documents.appendingPathComponent("userImage.png")
+        
+        UserImage.image = UIImage(contentsOfFile: imagePath.path)!
     }
     
     // MARK: - UIImagePickerControllerDelegate Methods
-     
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             UserImage.contentMode = .scaleAspectFit
             UserImage.image = pickedImage
+            
+            if let data = pickedImage.pngData() {
+                
+                let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                let url = documents.appendingPathComponent("userImage.png")
+                
+                do {
+                    try data.write(to: url)
+                    
+                } catch {
+                    print("Unable to Write Data to Disk (\(error))")
+                }
+            }
         }
-     
+        
         dismiss(animated: true, completion: nil)
     }
     
