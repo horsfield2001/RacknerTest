573fb49b598d5cc48384c0f6549eea4788ffc313
horsfield2001 <35241284+horsfield2001@users.noreply.github.com>
2020-08-12 20:41:15 -0700

    get image data and display blank cells

--- RacknerTest/CatImagesViewController.swift
+++ RacknerTest/CatImagesViewController.swift
@@ -11,7 +11,10 @@ import Alamofire
 
 class CatImagesViewController: UICollectionViewController {
     let apiKey = "eba04eae-3ca7-43f3-b768-9db7486925a2"
-    private let reuseIdentifier = "CatImageCell"
+    let reuseIdentifier = "CatImageCell"
+    var items: [Dictionary<String, Any>] = []
+    
+    private let itemsPerRow: CGFloat = 3
     private let sectionInsets = UIEdgeInsets(top: 50.0,
                                              left: 20.0,
                                              bottom: 50.0,
@@ -24,15 +27,70 @@ class CatImagesViewController: UICollectionViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        //https://api.thecatapi.com/v1/images/search?format=json&limit=100&page=0&size=thumb
+        
         AF.request("https://api.thecatapi.com/v1/images/search",
                    parameters: ["format": "json",
                                 "limit": 100,
                                 "page":0,
                                 "size":"thumb"],
                    headers: ["x-api-key": apiKey])
-            .responseJSON { (data) in
-                print(data)
+            .validate()
+            .responseJSON { (response) in
+                
+                guard let cats = response.value else { return }
+                
+                self.items = cats as! [Dictionary<String, Any>]
+                self.collectionView?.reloadData()
         }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension CatImagesViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView
+            .dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .black
+        // Configure the cell
+        return cell
+    }
+}
+
+// MARK: - Collection View Flow Layout Delegate
+extension CatImagesViewController : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
     }
 }
