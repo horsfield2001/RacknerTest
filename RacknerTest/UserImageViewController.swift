91249a13f72e3a81f7ad4cd34d3696d238af3e09
horsfield2001 <35241284+horsfield2001@users.noreply.github.com>
2020-08-12 22:37:16 -0700

    pick and display image from user's photos

--- RacknerTest/UserImageViewController.swift
+++ RacknerTest/UserImageViewController.swift
@@ -8,6 +8,38 @@
 
 import UIKit
 
-class UserImageViewController: UIViewController {
+class UserImageViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var UserImage: UIImageView!
+    
+    @IBAction func ShowImagePicker(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+            
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+   override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        imagePicker.delegate = self
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            UserImage.contentMode = .scaleAspectFit
+            UserImage.image = pickedImage
+        }
+     
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
     
 }
